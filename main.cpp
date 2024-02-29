a05dbac9fc2f9a2e55a85590c0689b96846c1ac6
sena@s73.work <Sergey>
2024-02-29 01:34:45 +0100

    выделил код в класс Block

--- main.cpp
+++ main.cpp
@@ -1,95 +1,103 @@
 #include <iostream>
 #include <stdexcept>
+#include <ctime>
 
-const int parsing_errorcode = 101;
+const int generic_errorcode = 101;
 
-void printBlock(std::string &blockoutput)
+class Block
 {
-  if(!blockoutput.empty())
+public:
+  // void parseLine(std::string && line)
+  // {
+  // }
+
+
+  void append(const std::string &line)
   {
-    std::cout << "bulk: "  << blockoutput << "\n";
-    blockoutput.clear();
+    if(m_cmdnum == 0) {
+      m_str = line;
+      m_timestamp = std::time(nullptr);
+    }
+    else {
+       m_str += ", " + line;
+    }
+    m_cmdnum++;
   }
-}
+
+  void print()
+  {
+    if(m_cmdnum > 0) {
+      std::cout << "bulk: "  << m_str << std::endl; // std::endl to flush the buffer
+      m_cmdnum = 0;
+    }
+  }
+
+  [[nodiscard]] unsigned cmdnum() const { return m_cmdnum;}
+
+private:
+  std::string m_str;
+  unsigned m_cmdnum{0};
+  time_t m_timestamp{};
+};
+
+
 
   // NOLINTBEGIN(cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
+  // NOLINTBEGIN(readability-braces-around-statements,hicpp-braces-around-statements)
   // NOLINTNEXTLINE(hicpp-named-parameter,readability-named-parameter)
 int main(int argc, char const *argv[] )
 {
-  int errorcode = 0;
-  int N{0};
+  try {
+    unsigned N{0};
 
-  if(argc == 2)
-  {
-    try {
-      N = std::stoi(argv[1]);
-    }
-    catch(const std::exception &e)
+    if(argc != 2)
     {
-      std::cerr << "Единственные параметр должен быть целым числом\n" << std::endl;
-      errorcode = parsing_errorcode;
+      std::cout << "Запуск:\n bulk <N>\n\n"
+                << " где <N> это количество команд в блоке для обработки\n";
+      return 0;
     }
-  }
-  if(argc != 2 || errorcode != 0)
-  {
-    std::cout << "Запуск:\n"
-              << argv[0] << " <N>\n\n"
-              << " где <N> это количество команд в блоке для обработки\n";
-    exit(errorcode);
-  }
 
-  int lineno{0};
-  int cmdnum{0};
-  std::string blockoutput;
-  int extendedModeLevel{0};
-  for(std::string line; std::getline(std::cin, line);++lineno)
-  {
     try {
+      N = std::stoul(argv[1]);
+    }
+    catch(...) {
+      throw std::runtime_error("Единственный аргумент командной строки дожен быть положительным числом");
+    }
+
+    int lineno{0};
+    Block block;
+    int extendedModeLevel{0};
+    for(std::string line; std::getline(std::cin, line);++lineno)
+    {
       if(line == "{")
       {
         extendedModeLevel++;
-        if(extendedModeLevel > 1)
-        {
-          continue;
-        }
-        printBlock(blockoutput);
-        cmdnum = 0;
+        if(extendedModeLevel > 1) continue;
+        block.print();
         continue;
       }
 
       if(line == "}")
       {
         extendedModeLevel--;
-        if(extendedModeLevel > 0)
-        {
-          continue;
-        }
-        if(extendedModeLevel < 0)
-        {
-          throw std::runtime_error("нарушен баланс скобок");
-        }
-        printBlock(blockoutput);
-        cmdnum = 0;
+        if(extendedModeLevel > 0) continue;
+        if(extendedModeLevel < 0) throw std::runtime_error("нарушен баланс скобок в строке " + std::to_string(lineno));
+        block.print();
         continue;
       }
 
-      if(cmdnum < N || extendedModeLevel > 0)
-      {
-        blockoutput += (cmdnum == 0) ? line : (", " + line);
-        cmdnum++;
-      }
-      if(cmdnum == N && extendedModeLevel == 0) {
-        printBlock(blockoutput);
-        cmdnum = 0;
-      }
+      if(block.cmdnum() < N || extendedModeLevel > 0) block.append(line);
+
+      if(block.cmdnum() == N && extendedModeLevel == 0) block.print();
     }
-    catch(const std::exception &e)
-    {
-      std::cerr << "Ошибка в строке " << lineno << ": " << e.what() << std::endl;
-      return parsing_errorcode;
-    }
+    block.print();
+    return 0;
   }
-  printBlock(blockoutput);
-  return 0;
+  catch(const std::exception &e)
+  {
+    std::cerr << "Ошибка\n" << e.what() << std::endl;
+    return generic_errorcode;
+  }
 }
+  // NOLINTEND(readability-braces-around-statements,hicpp-braces-around-statements)
   // NOLINTEND(cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
